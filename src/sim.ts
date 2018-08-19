2688849d66ff735ccf55945188c3aacd4f36b8a5
David Craven <david@craven.ch>
2018-08-19 21:01:30 +0200

    Sim takes ISimOptions.

--- src/sim.ts
+++ src/sim.ts
@@ -1,23 +1,43 @@
 import { execSync } from 'child_process'
+import { Triple } from './triple'
+import { Synth } from './tools/synth'
+import { getRelPath } from './relpath'
+
+export interface ISimOptions {
+    triple?: Triple
+    vpiSearchPath?: string[]
+    vpiModules?: string[]
+}
 
 export class Sim {
 
-    constructor(readonly searchPath: string[] = [],
-                readonly modules: string[] = []) {}
+    constructor(readonly options: ISimOptions = {}) {}
 
-    sim(input: string[], output: string): void {
-        this.iverilog(input, output)
+    sim(input: string[], output: string, top: string): void {
+        this.iverilog(input, output, top)
         this.vvp(output)
     }
 
-    iverilog(input: string[], output: string): void {
-        execSync(`iverilog -o ${output} ${input.join(' ')}`)
+    iverilog(input: string[], output: string, top: string): void {
+        if (this.options.triple) {
+            const synth = new Synth(this.options.triple)
+            input.push(synth.cellsPath())
+        }
+        execSync(`iverilog -s ${top} -o ${output} ${input.join(' ')}`)
     }
 
     vvp(input: string): void {
-        const spath = this.searchPath.join(':')
-        const marg = spath === '' ? '' : `-M ${spath}`
-        const mods = this.modules.map((m) => `-m ${m}`).join(' ')
-        execSync(`vvp ${marg} ${mods} ${input}`)
+        let options = ''
+        if (!this.options.vpiSearchPath) {
+            this.options.vpiSearchPath = [
+                getRelPath('vvp', '../lib/ivl')
+            ]
+        }
+        options += `-M ${this.options.vpiSearchPath.join(':')}`
+        if (this.options.vpiModules) {
+            options += this.options.vpiModules
+                .map((m) => `-m ${m}`).join(' ')
+        }
+        execSync(`vvp ${options} ${input}`)
     }
 }
