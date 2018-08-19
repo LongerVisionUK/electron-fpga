ff7675c63601e7a4399cfd90001adacc02fe61fd
David Craven <david@craven.ch>
2018-08-19 19:40:32 +0200

    prog takes device, interface and offset options.

--- src/tools/prog.ts
+++ src/tools/prog.ts
@@ -1,6 +1,18 @@
 import { execSync } from 'child_process'
 import { Triple } from '../triple'
 
+export interface IProgOptions {
+    offset?: string
+    device?: IUsbDevice
+    ftdiInterface?: 'A' | 'B' | 'C' | 'D'
+}
+
+export interface IUsbDevice {
+    vendor: string
+    product: string
+    serial?: string
+}
+
 export class Prog {
     static supportedFamilies = [ 'ice40' ]
 
@@ -10,7 +22,20 @@ export class Prog {
         }
     }
 
-    prog(file: string): void {
-        execSync(`iceprog ${file}`, { stdio: [ 0, 1, 2 ] })
+    prog(file: string, options: IProgOptions = {}): void {
+        let strOptions = ''
+        if (options.device) {
+            strOptions += `-d i` +
+                `:${options.device.vendor}` +
+                `:${options.device.product}` +
+                `:${options.device.serial || '0'}`
+        }
+        if (options.ftdiInterface) {
+            strOptions += `-I ${options.ftdiInterface}`
+        }
+        if (options.offset) {
+            strOptions += `-o ${options.offset}`
+        }
+        execSync(`iceprog ${strOptions} ${file}`, { stdio: [ 0, 1, 2 ] })
     }
 }
